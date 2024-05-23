c22a7cdefbe1848fd72c70e68627901b6fe08ff0
jefim <hi@jef.im>
2024-05-23 07:31:10 +0300

    WIP Avro.Serialize

---
+++ Frends.Avro.Serialize/Frends.Avro.Serialize/Definitions/Input.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Frends.Avro.Serialize.Definitions;
+
+/// <summary>
+/// Input parameters.
+/// </summary>
+public class Input
+{
+    /// <summary>
+    /// Input JSON string
+    /// </summary>
+    /// <example>{ "foo": "bar" }</example>
+    [DisplayFormat(DataFormatString = "Text")]
+    public string Json { get; set; }
+
+    /// <summary>
+    /// Avro Schema to use
+    /// </summary>
+    /// <example>
+    /// {
+    ///     "type": "record",
+    ///     "name":"Person",
+    ///     "fields":[
+    ///         { "name":"name", "type":"string" },
+    ///         { "name":"age","type":"int" },
+    ///         { "name":"city","type":"string" }
+    ///     ]
+    /// }
+    /// </example>
+    public string Schema { get; set; }
+}
---
+++ Frends.Avro.Serialize/Frends.Avro.Serialize/Definitions/Result.cs
@@ -0,0 +1,18 @@
+﻿namespace Frends.Avro.Serialize.Definitions;
+
+/// <summary>
+/// Task's result.
+/// </summary>
+public class Result
+{
+    /// <summary>
+    /// Operation complete without errors.
+    /// </summary>
+    /// <example>true</example>
+    public byte[] Avro { get; private set; }
+
+    internal Result(byte[] avro)
+    {
+        Avro = avro;
+    }
+}
